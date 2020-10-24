b53956f2ad2b3b517e9c9d16dd557730e5741721
Gustavo <gustavomaciel.dev@gmail.com>
2020-10-23 23:37:37 -0300

    Pedidos de confirmación, mensajes de resultado al usuario

--- app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/plant/DetailFragment.java
+++ app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/plant/DetailFragment.java
@@ -1,5 +1,7 @@
 package ar.edu.ort.bmon.rootsapp.ui.plant;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,19 +9,24 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
-import com.google.android.gms.common.internal.BaseGmsClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Date;
+import java.util.logging.Logger;
 
 import ar.edu.ort.bmon.rootsapp.R;
 import ar.edu.ort.bmon.rootsapp.model.Planta;
@@ -41,29 +48,13 @@ public class DetailFragment extends DialogFragment {
                              @Nullable Bundle savedInstanceState) {
 
         db = FirebaseFirestore.getInstance();
-//        DocumentReference docRef = db.collection("plantas").document("OaYKoR62sHciFQxxjUmY");
-//        docRef.get()
-//                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//                    @Override
-//                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-//                        if (documentSnapshot.exists()) {
-//                            System.out.println("Document data: " + documentSnapshot.getData());
-////                            planta.setAltura(documentSnapshot.get("altura").toString());
-////                            planta.setContenedor(documentSnapshot.get("contenedor").toString());
-//                        } else {
-//                            System.out.println("No such document!");
-//                        }
-//                    }
-//                });
-
-
-        final View root = inflater.inflate(R.layout.fragment_detail, container, false);
         DetailViewModel model = new ViewModelProvider(requireActivity()).get(DetailViewModel.class);
         planta = model.getSelected().getValue();
-        loadDetailValue(root);
+
+        final View root = inflater.inflate(R.layout.fragment_detail, container, false);
         Button editarBtn = root.findViewById(R.id.buttonEditarPlanta);
         Button eliminarBtn = root.findViewById(R.id.buttonEliminarPlanta);
-        Button modificarBtn = root.findViewById(R.id.buttonModificarPlanta);;
+        Button modificarBtn = root.findViewById(R.id.buttonSavePlanta);;
 
         editarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,58 +65,112 @@ public class DetailFragment extends DialogFragment {
         eliminarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                eliminarPlanta(planta);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(R.string.dialog_delete)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                eliminarPlanta(root, planta);
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+//                                TAG.
+                                System.out.println("Canceló eliminar");
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
             }
         });
         modificarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                modificarPlanta(root);
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(R.string.dialog_edit)
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                savePlanta(root);
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+//                                TAG.
+                                // User cancelled the dialog
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
             }
         });
+        loadDetailValue(root);
         return root;
     }
 
-    private void modificarPlanta(View root) {
-        DocumentReference docRef = db.collection("plantas").document("c3TMhIdzjxkg1vJAyzLR");
-        Planta planta = new Planta();
-        EditText editTextAltura = root.findViewById(R.id.editTextAltura);
-        editTextAltura.getText();
-        planta.setAltura(editTextAltura.getText().toString());
+    private void savePlanta(View root) {
+        DocumentReference docRef = db.collection("plantas").document(planta.getId());
+
+        EditText altura = root.findViewById(R.id.editTextAltura);
+        planta.setAltura(altura.getText().toString());
+
+        EditText contenedor = (EditText)root.findViewById(R.id.editTextContenedor);
+        planta.setContenedor(contenedor.getText().toString());
+
+        EditText origen = (EditText)root.findViewById(R.id.editTextOrigen);
+        planta.setOrigen(origen.getText().toString());
+
+        EditText edad = (EditText)root.findViewById(R.id.editTextEdad);
+        planta.setEdad(edad.getText().toString());
+
+        EditText fechaRegistro = (EditText)root.findViewById(R.id.editTextFechaRegistro);
+        planta.setFechaRegistro(new Date());
+
+        Switch aptoBonsai = (Switch) root.findViewById(R.id.switchAptoBonsai);
+        planta.setAptoBonzai(aptoBonsai.isChecked());
+
+        Switch aptoVenta = (Switch) root.findViewById(R.id.switchAptoVenta);
+        planta.setAptoVenta(aptoVenta.isChecked());
+
+
         docRef.set(planta)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        //TODO mostrar msj OK y volver al listado
                         System.out.println("Planta Modificada");
+                        Toast.makeText(getContext(), R.string.msj_modificacion_ok, 3500).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        //TODO mostrar msj de error en popup y luego volver al listado de plantas
                         System.out.println("Error al modificar Planta" + " " + e.getCause());
+                        Toast.makeText(getContext(), R.string.msj_modificacion_error, 3500);
+
                     }
                 });
+        Navigation.findNavController(root).navigate(R.id.nav_home);
     }
 
-    private void eliminarPlanta(Planta planta) {
+    private void eliminarPlanta(final View root, Planta planta) {
         DocumentReference docRef = db.collection("plantas").document(planta.getId());
         docRef.delete()
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        //TODO mostrar msj OK y volver al listado
                         System.out.println("Documento eliminado");
+                        Toast.makeText(getContext(), R.string.msj_eliminar_ok, 3500).show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        //TODO mostrar msj de error en popup y luego volver al listado de plantas
                         System.out.println("Error al intentar eliminar");
+                        Toast.makeText(getContext(), R.string.msj_eliminar_error, 3500).show();
                     }
                 });
+        Navigation.findNavController(root).navigate(R.id.nav_home);
+
 
 
     }
@@ -138,6 +183,9 @@ public class DetailFragment extends DialogFragment {
         root.findViewById(R.id.editTextFechaRegistro).setEnabled(true);
         root.findViewById(R.id.switchAptoBonsai).setEnabled(true);
         root.findViewById(R.id.switchAptoVenta).setEnabled(true);
+        root.findViewById(R.id.buttonSavePlanta).setVisibility(View.VISIBLE);
+        root.findViewById(R.id.buttonEliminarPlanta).setVisibility(View.VISIBLE);
+        root.findViewById(R.id.buttonEditarPlanta).setVisibility(View.INVISIBLE);
     }
 
     private void loadDetailValue(View root){
@@ -174,8 +222,6 @@ public class DetailFragment extends DialogFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        DetailViewModel model = new ViewModelProvider(requireActivity()).get(DetailViewModel.class);
-//        planta = model.getSelected().getValue();
 
     }
 }
