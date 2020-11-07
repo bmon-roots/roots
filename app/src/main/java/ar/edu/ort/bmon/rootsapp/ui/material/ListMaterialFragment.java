d87b6d5b4e51458aa17c8d5f7232a4d317bfd848
juanbori <jsires94@hotmail.com>
2020-11-07 17:31:32 -0300

    Se agrega dialog de material con un layout adentro.

--- app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/material/ListMaterialFragment.java
+++ app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/material/ListMaterialFragment.java
@@ -3,23 +3,27 @@ package ar.edu.ort.bmon.rootsapp.ui.material;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import ar.edu.ort.bmon.rootsapp.R;
-import ar.edu.ort.bmon.rootsapp.ui.report.ReportViewModel;
 
 public class ListMaterialFragment extends Fragment {
 
-    private ListMaterialViewModel listMaterialViewModel;
+    private RecyclerView recyclerView;
+    private FloatingActionButton btnAddAction;
 
     public static ListMaterialFragment newInstance() {
         return new ListMaterialFragment();
@@ -28,23 +32,40 @@ public class ListMaterialFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        listMaterialViewModel =
-                ViewModelProviders.of(this).get(ListMaterialViewModel.class);
         View root = inflater.inflate(R.layout.fragment_list_material, container, false);
-        final TextView textView = root.findViewById(R.id.textView_material);
-        listMaterialViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;    }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        listMaterialViewModel = ViewModelProviders.of(this).get(ListMaterialViewModel.class);
-        // TODO: Use the ViewModel
+        recyclerView = root.findViewById(R.id.recyclerMateriales);
+        btnAddAction = root.findViewById(R.id.addMaterialBtn);
+        return root;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnAddAction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext());
+                builder.setBackground(getResources().getDrawable(R.drawable.alert_dialog_bg, null));
+                builder.setTitle("Ingrese un nuevo Material");
+//                builder.setMessage("HOLI");
+                View material = getLayoutInflater().inflate(R.layout.material_item, null);
+                builder.setView(material);
+                builder.setPositiveButton("Agregar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
+    }
 }
