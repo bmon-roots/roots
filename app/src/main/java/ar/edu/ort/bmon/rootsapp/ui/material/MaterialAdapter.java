d033c910129ec481db69a495c0f1a549d8a0978a
juanbori <jsires94@hotmail.com>
2020-11-07 21:49:19 -0300

    suma y resta de materiales funcionando

--- app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/material/MaterialAdapter.java
+++ app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/material/MaterialAdapter.java
@@ -4,30 +4,90 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 import ar.edu.ort.bmon.rootsapp.R;
+import ar.edu.ort.bmon.rootsapp.constants.Constants;
 import ar.edu.ort.bmon.rootsapp.model.Material;
 
 
 public class MaterialAdapter extends FirestoreRecyclerAdapter<Material, MaterialAdapter.MaterialHolder> {
+
+    public DocumentSnapshot document;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public MaterialAdapter(@NonNull FirestoreRecyclerOptions<Material> options) {
         super(options);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull MaterialHolder holder, int position, @NonNull Material model) {
+    protected void onBindViewHolder(@NonNull final MaterialHolder holder, int position, @NonNull final Material model) {
+        document = getSnapshots().getSnapshot(holder.getAdapterPosition());
+        final DocumentReference docRef = db.collection(Constants.MATERIAL_COLLECTION).document(document.getId());
+
         holder.material.setText(model.getTipoMaterial().name());
         holder.contenido.setText(String.valueOf(model.getContenido()) + "L");
-        holder.cantidad.setText(String.valueOf(model.getCantidad()) + " unidades");
+        holder.cantidad.setText(String.valueOf(model.getCantidad()));
+
+        holder.substractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                docRef.update(
+                        "cantidad", model.getCantidad() - 1,
+                        "contenido", model.getContenido(),
+                        "tipoMaterial", model.getTipoMaterial().name()
+                )
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                System.out.println("SE RESTO UNO");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                System.out.println("Error al restar uno");
+                            }
+                        });
+            }
+        });
+
+        holder.addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                docRef.update(
+                        "cantidad", model.getCantidad() + 1,
+                        "contenido", model.getContenido(),
+                        "tipoMaterial", model.getTipoMaterial().name()
+                )
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                System.out.println("SE SUMO UNO");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                System.out.println("Error al restar uno");
+                            }
+                        });
+            }
+        });
+
     }
 
     @NonNull
@@ -35,20 +95,6 @@ public class MaterialAdapter extends FirestoreRecyclerAdapter<Material, Material
     public MaterialHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.material_item, parent, false);
         final MaterialHolder mHolder = new MaterialHolder(view);
-        mHolder.substractButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        mHolder.addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
 
         return mHolder;
     }
