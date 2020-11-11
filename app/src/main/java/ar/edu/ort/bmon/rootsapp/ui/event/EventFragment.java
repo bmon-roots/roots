cdf249a83cb266498cae78a56076312b326b6e22
Gustavo <gustavomaciel.dev@gmail.com>
2020-11-10 23:25:26 -0300

    add event option to menu

---
+++ app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/event/EventFragment.java
@@ -0,0 +1,50 @@
+package ar.edu.ort.bmon.rootsapp.ui.event;
+
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import ar.edu.ort.bmon.rootsapp.R;
+import ar.edu.ort.bmon.rootsapp.ui.report.ReportViewModel;
+
+public class EventFragment extends Fragment {
+
+    private EventViewModel mViewModel;
+
+    public static EventFragment newInstance() {
+        return new EventFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mViewModel = ViewModelProviders.of(this).get(EventViewModel.class);
+        View root = inflater.inflate(R.layout.event_fragment, container, false);
+        final TextView title = root.findViewById(R.id.text_report);
+        mViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+                    @Override
+                    public void onChanged(String s) {
+                        title.setText(s);
+                    }
+                });
+        return root;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(EventViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
