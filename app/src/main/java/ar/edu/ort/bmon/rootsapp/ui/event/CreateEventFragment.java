7c9ad70cc9e5c6568d59444ae17c3afe3c4ae0f0
Felipe Ortiz <ortiz.jfelipe@gmail.com>
2020-11-15 00:43:18 -0300

    Se agrega opcion de guardado en el menu, se agregan elementos al archivo strings.xml, se agrega la logica para el guardado del evento a firebase

--- app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/event/CreateEventFragment.java
+++ app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/event/CreateEventFragment.java
@@ -12,6 +12,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -27,9 +30,16 @@ public class CreateEventFragment extends Fragment {
     private TextView selectedEventId;
     private ImageView eventImage;
     private Fragment fragmentOptions;
+    private int selectedOption;
 
     public static CreateEventFragment newInstance() {
         return new CreateEventFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -39,13 +49,36 @@ public class CreateEventFragment extends Fragment {
         String[] eventOptions = new String[] { Constants.GERMINATION, Constants.CUTTING };
         selectedEventId = viewReference.findViewById(R.id.textViewSelectedEventType);
         eventImage = viewReference.findViewById(R.id.eventImageView);
-        int selectedOption = Integer.parseInt(getArguments().getString(Constants.SELECTED_EVENT));
+        selectedOption = Integer.parseInt(getArguments().getString(Constants.SELECTED_EVENT));
         fragmentOptions = initFragmentOptions(selectedOption);
         getChildFragmentManager().beginTransaction().add(R.id.frameOptionsLayout, fragmentOptions).commit();
         selectedEventId.setText(eventOptions[selectedOption]);
         Uri eventImageUri = getImageForEventType(selectedOption);
         eventImage.setImageURI(eventImageUri);
         return viewReference;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_create_event, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(CreateEventViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        saveEventToFirebase(selectedOption);
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void saveEventToFirebase(int selectedOption) {
+
     }
 
     private Fragment initFragmentOptions(int selectedOption) {
@@ -58,10 +91,4 @@ public class CreateEventFragment extends Fragment {
         return Uri.parse("android.resource://ar.edu.ort.bmon.rootsapp/drawable/" + imageForEvent);
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(CreateEventViewModel.class);
-        // TODO: Use the ViewModel
-    }
 }
