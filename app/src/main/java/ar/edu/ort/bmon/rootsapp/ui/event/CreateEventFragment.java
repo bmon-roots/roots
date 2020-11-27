a9ab4cd9440c9d552fb7020872549f5ce9bde125
Felipe Ortiz <ortiz.jfelipe@gmail.com>
2020-11-27 18:56:58 -0300

    Se arreglan los problemas con el alta de eventos, se agrega la posibilidad de fechas futuras en el alta de eventos para las fechas posibles de germinacion y estratificacion

--- app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/event/CreateEventFragment.java
+++ app/src/main/java/ar/edu/ort/bmon/rootsapp/ui/event/CreateEventFragment.java
@@ -305,7 +305,7 @@ public class CreateEventFragment extends Fragment {
     }
 
     private void showDatePickerDialog(FragmentManager fragmentManager) {
-        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(true, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 // +1 because January is zero
